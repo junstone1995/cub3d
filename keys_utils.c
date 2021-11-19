3ff1c642dba9b3532cf6ddea7d4b7eb6ab04aa02
NamHyeop Kim <skaguq@gmail.com>
2021-11-19 18:19:27 +0900

    add file namhyeop

---
+++ keys_utils.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keys_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namhkim <namhkim@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/19 16:16:11 by namhkim           #+#    #+#             */
+/*   Updated: 2021/11/19 16:58:36 by namhkim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	get_speed1(int playerSpeed)
+{
+	if (playerSpeed > 1)
+		return (playerSpeed - 1);
+	else
+		return (0);
+}
+
+int	get_speed2(int playerSpeed)
+{
+	if (playerSpeed < 10)
+		return (playerSpeed + 1);
+	else
+		return (0);
+}
